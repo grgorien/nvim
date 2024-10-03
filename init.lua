0b6faea761559280ff4191795b35d1640e755431
William Gregorio <mail@williamgregorio.com>
2024-10-03 01:14:12 -0400

    inti

--- init.lua
+++ init.lua
@@ -1,17 +1,21 @@
-require('william.plugins-setup')
-require('william.core.options')
-require('william.core.keymaps')
-require('william.core.colorscheme')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
-require("william.plugins.comment")
-require("william.plugins.nvim-tree")
-require("william.plugins.lualine")
-require("william.plugins.telescope")
-require("william.plugins.nvim-cmp")
-require("william.plugins.lsp.mason")
-require("william.plugins.lsp.lspsaga")
-require("william.plugins.lsp.lspconfig")
-require("william.plugins.lsp.null-ls")
-require("william.plugins.autopairs")
-require("william.plugins.treesitter")
-require("william.plugins.gitsigns")
+
+require("core.options")
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
---
+++ lua/core/plugin_config/luasnip.lua
@@ -0,0 +1,181 @@
+local ls = require "luasnip"
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+require("luasnip.config").setup({
+  history = true,
+  updateevents = "TextChanged,TextChangedI",
+  enable_autosnippets = true,
+  -- Add more config options if needed
+  require("luasnip.loaders.from_vscode").lazy_load({paths = { vim.fn.stdpath("config") .. "/snippets/nvim-mjml" },})
+})
+
+vim.cmd('autocmd BufRead,BufNewFile *.mjml setfiletype mjml')
+
+ls.add_snippets("lua", {
+  s("test", fmt(
+    [[
+    hello im {}
+    ]], {i(1)}
+  ))
+})
+
+ls.add_snippets("mjml", {
+  s("test", fmt([[ 
+    hello mjml im {}
+    ]], {i(1)}))
+})
+
+vim.keymap.set({"i","s"}, "<Tab>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true }
+)
+
+vim.keymap.set({"i", "s"}, "<S-Tab>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true }
+)
+
+ls.add_snippets("mjml", {
+  s("mjml", fmt([[
+    <mjml>
+      <mj-head>
+        {head}
+      </mj-head>
+      <mj-body>
+        {body}
+      </mj-body>
+    </mjml>
+  ]], {
+    head = i(1, "<!-- Add head content here -->"),
+    body = i(2, "<!-- Add body content here -->"),
+  })),
+
+  -- Standard Head Components
+  s("mjattributes", fmt([[
+    <mj-attributes>
+      {attributes}
+    </mj-attributes>
+  ]], { attributes = i(1, "/* Add attributes here */") })),
+  s("mjbreakpoint", fmt([[
+    <mj-breakpoint width="{width}" />
+  ]], { width = i(1, "600px") })),
+  s("mjfont", fmt([[
+    <mj-font name="{name}" href="{href}" />
+  ]], { name = i(1, "FontName"), href = i(2, "https://font-url.com") })),
+  s("mjhtmlattributes", fmt([[
+    <mj-html-attributes>
+      {attributes}
+    </mj-html-attributes>
+  ]], { attributes = i(1, "/* Add HTML attributes here */") })),
+  s("mjpreview", fmt([[
+    <mj-preview>
+      {text}
+    </mj-preview>
+  ]], { text = i(1, "Preview text here") })),
+  s("mjstyle", fmt([[
+    <mj-style>
+      {styles}
+    </mj-style>
+  ]], { styles = i(1, "/* Add styles here */") })),
+  s("mjtitle", fmt([[
+    <mj-title>
+      {title}
+    </mj-title>
+  ]], { title = i(1, "Title here") })),
+
+  -- Standard Body Components
+  s("mjaccordion", fmt([[
+    <mj-accordion>
+      {content}
+    </mj-accordion>
+  ]], { content = i(1, "<!-- Add accordion content here -->") })),
+  s("mjbutton", fmt([[
+    <mj-button>
+      {text}
+    </mj-button>
+  ]], { text = i(1, "<!-- Add button text here -->") })),
+  s("mjcarousel", fmt([[
+    <mj-carousel>
+      {content}
+    </mj-carousel>
+  ]], { content = i(1, "<!-- Add carousel content here -->") })),
+  s("mjcolumn", fmt([[
+    <mj-column width="{width}">
+      {content}
+    </mj-column>
+  ]], { width = i(1, "600px"), content = i(2, "<!-- Add column content here -->") })),
+  s("mjdivider", fmt([[
+    <mj-divider {attributes} />
+  ]], { attributes = i(1, "/* Add divider attributes here */") })),
+  s("mjgroup", fmt([[
+    <mj-group>
+      {content}
+    </mj-group>
+  ]], { content = i(1, "<!-- Add group content here -->") })),
+  s("mjhero", fmt([[
+    <mj-hero>
+      {content}
+    </mj-hero>
+  ]], { content = i(1, "<!-- Add hero content here -->") })),
+  s("mjimage", fmt([[
+    <mj-image src="{src}" alt="{alt}" />
+  ]], { src = i(1, "https://image-url.com"), alt = i(2, "Image description") })),
+  s("mjnavbar", fmt([[
+    <mj-navbar>
+      {content}
+    </mj-navbar>
+  ]], { content = i(1, "<!-- Add navbar content here -->") })),
+  s("mjraw", fmt([[
+    <mj-raw>
+      {content}
+    </mj-raw>
+  ]], { content = i(1, "<!-- Add raw HTML here -->") })),
+  s("mjsection", fmt([[
+    <mj-section>
+      {content}
+    </mj-section>
+  ]], { content = i(1, "<!-- Add section content here -->") })),
+  s("mjsocial", fmt([[
+    <mj-social>
+      {content}
+    </mj-social>
+  ]], { content = i(1, "<!-- Add social content here -->") })),
+  s("mjspacer", fmt([[
+    <mj-spacer height="{height}" />
+  ]], { height = i(1, "20px") })),
+  s("mjtable", fmt([[
+    <mj-table>
+      {content}
+    </mj-table>
+  ]], { content = i(1, "<!-- Add table content here -->") })),
+  s("mjtext", fmt([[
+    <mj-text>
+      {content}
+    </mj-text>
+  ]], { content = i(1, "<!-- Add text content here -->") })),
+  s("mjwrapper", fmt([[
+    <mj-wrapper>
+      {content}
+    </mj-wrapper>
+  ]], { content = i(1, "<!-- Add wrapper content here -->") })),
+
+  -- Ending tags (demonstration of placeholders in ending tags)
+  s("mjtextend", fmt([[
+    <mj-text>
+      {content}
+    </mj-text>
+  ]], { content = i(1, "<!-- Add text content here -->") })),
+  s("mjbuttonend", fmt([[
+    <mj-button>
+      {text}
+    </mj-button>
+  ]], { text = i(1, "<!-- Add button text here -->") })),
+})
