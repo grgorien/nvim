9ca2d3e4ac78399cdf42ae7df0ab200fd4d70705
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-18 09:49:34 -0400

    mv options to here

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,21 +1,43 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.opt.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
-vim.opt.backspace = '2'
-vim.opt.showcmd = true
 vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
+vim.opt.showcmd = true
 
--- use spaces for tabs and whatnot
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.wo.number = true
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.cindent = true
+
+vim.opt.wrap = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
 vim.cmd [[ set noswapfile ]]
 vim.cmd [[ set termguicolors ]]
 
---Line numbers
-vim.wo.number = true
