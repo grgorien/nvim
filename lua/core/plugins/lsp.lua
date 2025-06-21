958271bd2823a064fbb9db486e08e133a4008f8f
William <mrwilliamgregorio@yahoo.com>
2025-06-21 01:32:21 -0400

    Add lsp for current langs

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -1,21 +1,59 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            {
-                "folke/lazydev.nvim",
-                ft = "lua", -- only load on lua files
-                opts = {
-                    library = {
-                        -- See the configuration section for more details
-                        -- Load luvit types when the `vim.uv` word is found
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                    },
-                },
-            },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
-        config = function()
-            require('lspconfig').lua_ls.setup{}
-        end,
-    }
+      },
+    },
+    config = function()
+      local lspconfig = require('lspconfig')
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      lspconfig.lua_ls.setup{
+        capabilities = capabilities,
+      }
+      lspconfig.gopls.setup{
+        capabilities = capabilities,
+      }
+      lspconfig.html.setup{
+        capabilities = capabilities,
+      }
+      lspconfig.cssls.setup{
+        capabilities = capabilities,
+      }
+      lspconfig.ts_ls.setup{
+        capabilities = capabilities,
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = "/usr/lib/node_modules/typescript-plugin",
+              languages = { "javascript", "typescript", "vue" },
+            }
+          }
+        },
+        filetypes = {
+          "javascript",
+          "typescript",
+          "vue"
+        },
+      }
+      lspconfig.vue_ls.setup{
+        capabilities = capabilities,
+        filetypes = { "vue" }
+      }
+      lspconfig.pyright.setup{
+        capabilities = capabilities,
+      }
+    end,
+  }
 }
