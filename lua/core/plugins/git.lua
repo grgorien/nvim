d3c1e38e0b45ad1280e27422cf0d248be947a45b
William <mrwilliamgregorio@yahoo.com>
2025-06-21 01:00:04 -0400

    Add gitsigns with some mappings for hunk support and blaming

--- lua/core/plugins/git.lua
+++ lua/core/plugins/git.lua
@@ -7,5 +7,35 @@ return {
 
       "nvim-telescope/telescope.nvim"
     }
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+          map("n", "]c", gs.next_hunk)
+          map("n", "c]", gs.prev_hunk)
+          map("n", "<leader>hs", gs.stage_hunk)
+          map("n", "<leader>hr", gs.reset_hunk)
+          map("n", "<leader>hp", gs.preview_hunk)
+          map("n", "<leader>hb", gs.blame_line)
+        end,
+      })
+    end,
   }
 }
