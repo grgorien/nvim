e0bc7b836b451b79dde8ea176ec313cc9ab73e5a
William Gregorio <william.gregorio.ny@gmail.com>
2025-10-08 22:54:37 -0400

    purp force on colorschem

--- lua/core/plugins/joker.lua
+++ lua/core/plugins/joker.lua
@@ -4,5 +4,11 @@ return {
   priority = 1000, -- to load before other plugins
   config = function()
     vim.cmd('colorscheme joker')
+
+    vim.api.nvim_set_hl(0, "Visual", {
+      bg = "#553399",
+      fg = "#FFFFFF",
+      bold = true
+    })
   end,
 }
