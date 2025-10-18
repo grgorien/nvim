20b0e3aa0bbf513b6c5b27e7e0057e1235106bf3
William Gregorio <william.gregorio.ny@gmail.com>
2025-10-18 03:27:54 -0400

    fix(gopls): correct root_dir detection to stop workspace error

--- lua/core/lsp/gopls.lua
+++ lua/core/lsp/gopls.lua
@@ -4,7 +4,7 @@ return {
   name = 'gopls',
   cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = vim.fs.find({ "go.mod", "go.work", ".git" }, { upward = true })[1],
+  root_dir = vim.fs.dirname(vim.fs.find({ "go.mod", "go.work", ".git" }, { upward = true })[1]),
   settings = {},
   capabilities = lsp.make_capabilities(),
   on_attach = lsp.on_attach,
