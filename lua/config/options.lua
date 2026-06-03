343d4c123470155247d6363e4d78a4f440cc35f8
William Gregorio <william.gregorio.ny@gmail.com>
2026-06-03 13:56:09 -0400

    windows support if linux you must change

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,18 @@
 
 vim.g.lazyvim_lsp_inlay_hints = false
 vim.o.updatetime = 250
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
